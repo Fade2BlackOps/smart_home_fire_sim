8dce0fbb020c7d18117aa3110e953c77f3bed60a
VTIvanovIII <vtivanoviii5757@eagle.fgcu.edu>
2026-01-29 01:11:07 -0500

    Update NUM_SENSORS to 9 and clarify FIRE_ROOM comment

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-NUM_SENSORS = 6
+NUM_SENSORS = 9
 BASE_TEMP = 22.0         # °C
 TIME_STEPS = 500
-FIRE_ROOM = 1
+FIRE_ROOM = 1            # Should be between 0 and NUM_SENSORS-1
 FIRE_START = 300         # Time step when fire starts
 ALERT_THRESHOLD = 55.0   # °C
 VOTE_QUORUM = 5          # Minimum votes to confirm fire (out of NUM_SENSORS)
